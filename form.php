5afc6dd901f3481b530f7861a9cf1bb64a32200f
Anis Subba <21302329@student.uwl.ac.uk>
2020-02-11 22:56:50 +0000

    improving the form

--- form.php
+++ form.php
@@ -1,9 +1,52 @@
 <?php
-    if (isset($_POST['searchterm'])){
-        echo htmlspecialchars ($_POST['searchterm'], ENT_QUOTES);
-    }
-?>
+    $name = '';
+    $password = '';
+    $gender = '';
+    $color = '';
+    $languages = [];
+    $comments = '';
+    $tc = '';
 
+    if (isset($_POST['submit'])){
+        // echo htmlspecialchars ($_POST['searchterm'], ENT_QUOTES);
+        if (isset($_POST['name'])){
+            $name = $_POST['name'];
+        };
+        if (isset($_POST['password'])){
+            $password = $_POST['password'];
+        };
+        if (isset($_POST['gender'])){
+            $gender = $_POST['gender'];
+        };
+        if (isset($_POST['color'])){
+            $color = $_POST['color'];
+        };
+        if (isset($_POST['languages'])){
+            $languages = $_POST['languages'];
+        };
+        if (isset($_POST['comments'])){
+            $comments = $_POST['comments'];
+        };
+        if (isset($_POST['tc'])){
+            $tc = $_POST['tc'];
+        };
+
+        printf('User name; %s
+            <br>Password: %s
+            <br>Gender: %s
+            <br>Color: %s
+            <br>Language(s): %s
+            <br>Comments: %s
+            <br>T&amp;C: %s',
+            htmlspecialchars($name, ENT_QUOTES),
+            htmlspecialchars($password, ENT_QUOTES),
+            htmlspecialchars($gender, ENT_QUOTES),
+            htmlspecialchars($color, ENT_QUOTES),
+            htmlspecialchars(implode(' ', $languages, ENT_QUOTES)),
+            htmlspecialchars($comments, ENT_QUOTES),
+            htmlspecialchars($tc, ENT_QUOTES));
+        }
+?>
 <form
     action=""
     method="post">
@@ -28,5 +71,5 @@
         </select><br>
     Comments: <textarea name = "comments"></textarea><br>
     <input type = "checkbox" name = "tc" value = "ok">I accept the T&amp;C
-    <input type = "submit" value = "Search">
+    <input type = "submit" name = "submit" value = "Register">
     </form>
