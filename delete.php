faa3f31163ec1ada88811651f1496c4170cf0bfe
Anis Subba <ASubba@spartaglobal.com>
2020-02-12 14:19:31 +0000

    delete data from database

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+    //delete.php?id=2
+    if (isset($_GET['id']) && ctype_digit($_GET['id'])){
+        $id = $_GET['id'];
+    } else {
+        header('Location:select.php');
+    }
+
+    $db = new mysqli(
+        'localhost',
+        'root',
+        '',
+        'php'
+    );
+
+    $sql = "DELETE FROM users WHERE id=$id";
+    $db->query($sql);
+    echo '<p>User Deleted.</p>';
+    $db->close();
+?>
